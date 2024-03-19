eef3368bf463a79eaf1357b2db448be163ee0bf8
GitHub Action <action@github.com>
2024-03-19 16:43:59 +0000

    Template cleanup

--- src/main/kotlin/com/github/smwlucaso/djangoormvalidator/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/smwlucaso/djangoormvalidator/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.smwlucaso.djangoormvalidator.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.smwlucaso.djangoormvalidator.MyBundle
+import com.github.smwlucaso.djangoormvalidator.services.MyProjectService
 import javax.swing.JButton
 
 
